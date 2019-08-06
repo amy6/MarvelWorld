8f4e769f9b7a9529ed638ee9a8c9223487dc4144
Mahima <mahima.6.ms@gmail.com>
2019-08-06 21:25:17 +0530

    migrate to androidx

--- app/src/main/java/com/example/marvelworld/MarvelDetailActivity.java
+++ app/src/main/java/com/example/marvelworld/MarvelDetailActivity.java
@@ -1,6 +1,6 @@
 package com.example.marvelworld;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MarvelDetailActivity extends AppCompatActivity {
