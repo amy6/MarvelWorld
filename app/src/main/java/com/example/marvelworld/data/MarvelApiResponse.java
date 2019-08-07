75ddea64a4423d186caff1b5b583cce0c12abc77
Mahima <mahima.6.ms@gmail.com>
2019-08-07 17:09:35 +0530

    set up pojo classes to parse API response

---
+++ app/src/main/java/com/example/marvelworld/data/MarvelApiResponse.java
@@ -0,0 +1,65 @@
+package com.example.marvelworld.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MarvelApiResponse {
+
+        @SerializedName("offset")
+        @Expose
+        private Integer offset;
+        @SerializedName("limit")
+        @Expose
+        private Integer limit;
+        @SerializedName("total")
+        @Expose
+        private Integer total;
+        @SerializedName("count")
+        @Expose
+        private Integer count;
+        @SerializedName("results")
+        @Expose
+        private List<MarvelCharacter> marvelCharacters = null;
+
+        public Integer getOffset() {
+            return offset;
+        }
+
+        public void setOffset(Integer offset) {
+            this.offset = offset;
+        }
+
+        public Integer getLimit() {
+            return limit;
+        }
+
+        public void setLimit(Integer limit) {
+            this.limit = limit;
+        }
+
+        public Integer getTotal() {
+            return total;
+        }
+
+        public void setTotal(Integer total) {
+            this.total = total;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void setCount(Integer count) {
+            this.count = count;
+        }
+
+        public List<MarvelCharacter> getMarvelCharacters() {
+            return marvelCharacters;
+        }
+
+        public void setResults(List<MarvelCharacter> marvelCharacters) {
+            this.marvelCharacters = marvelCharacters;
+        }
+}
---
+++ app/src/main/java/com/example/marvelworld/data/Series.java
@@ -0,0 +1,55 @@
+package com.example.marvelworld.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Series {
+
+    @SerializedName("available")
+    @Expose
+    private Integer available;
+    @SerializedName("collectionURI")
+    @Expose
+    private String collectionURI;
+    @SerializedName("items")
+    @Expose
+    private List<SeriesItem> items = null;
+    @SerializedName("returned")
+    @Expose
+    private Integer returned;
+
+    public Integer getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Integer available) {
+        this.available = available;
+    }
+
+    public String getCollectionURI() {
+        return collectionURI;
+    }
+
+    public void setCollectionURI(String collectionURI) {
+        this.collectionURI = collectionURI;
+    }
+
+    public List<SeriesItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<SeriesItem> items) {
+        this.items = items;
+    }
+
+    public Integer getReturned() {
+        return returned;
+    }
+
+    public void setReturned(Integer returned) {
+        this.returned = returned;
+    }
+
+}
